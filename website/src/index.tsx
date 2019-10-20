17cc1da7da4a950d4c7ac839e70e09b9d67f9a75
Your Name <you@example.com>
2019-10-20 21:44:23 +0200

    work

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -4,9 +4,7 @@ import cssResetStyles from '@atlaskit/css-reset';
 import 'regenerator-runtime/runtime';
 import insertStyleSheetInHead from './utils/insertStyleSheetInHead';
 import App from './containers/App';
-import Test from './sitemap-generator';
 
-Test();
 
 insertStyleSheetInHead(cssResetStyles);
 
